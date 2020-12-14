cd261b4e4d6378be3cbf24fb771b557c17216737
spf34 <samuel.forster@cantab.net>
2020-12-14 22:14:36 +0000

    refactor Calculator and test

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,12 @@
-function addition() {
-    return 42;
+Calculator = function() {
+    this.value = 0;
+}
+
+Calculator.prototype.add = function(number){
+    if(typeof(number) == "number"){
+        this.value += number;
+    }
+    else{
+        alert("Error!");
+    }
 }
